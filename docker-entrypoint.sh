6b1d6cf75c5b16586d126dcf89fa8368a814e995
EntropyPool <69444208+EntropyPool@users.noreply.github.com>
2021-11-16 18:21:36 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 100000000
+
 CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=minio.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=minio.npool.top -port=9000
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO REGISTER ME TO CONSUL"
